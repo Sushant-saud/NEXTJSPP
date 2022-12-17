d8be3dfb2c97fa411811977bbd9b32be72fb3f96
Sushant-saud <sushantsaud9804@gmail.com>
2022-12-17 21:37:46 +0530

    merge done

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,7 @@ export default function Contact() {
    <Title title="Contact" />
    <h1>Contact Page</h1>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem inventore aperiam veniam impedit quas necessitatibus quis tempore delectus recusandae dolores animi nostrum sequi suscipit dolor ut adipisci, officiis minima?</p>
+   <p>Just try to understand the concept of branching and marging</p>
   </>
  )
 }
