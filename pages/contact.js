20d42cd5d9216c432751c8545467786181518b82
Sushant-saud <sushantsaud9804@gmail.com>
2022-12-23 18:48:28 +0530

    add img

--- pages/contact.js
+++ pages/contact.js
@@ -6,6 +6,7 @@ export default function Contact() {
    <h1>Contact Page</h1>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem inventore aperiam veniam impedit quas necessitatibus quis tempore delectus recusandae dolores animi nostrum sequi suscipit dolor ut adipisci, officiis minima?</p>
    <p>Just try to understand the concept of branching and marging</p>
+   <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/b7/b3/eb/caption.jpg?w=1200&h=-1&s=1" alt="img"/>
   </>
  )
 }
