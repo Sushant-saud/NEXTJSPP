d0a7484aafa9124ecb8c779cbb4b94229e198948
Sushant-saud <sushantsaud9804@gmail.com>
2022-12-23 19:06:02 +0530

    img add

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,8 @@ export default function About() {
   <>
    <Title title="About" />
    <h1>About Page</h1>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem inventore aperiam veniam impedit quas necessitatibus quis tempore delectus recusandae dolores animi nostrum sequi suscipit dolor ut adipisci, officiis minima?</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem inventore aperiam veniam impedit q uas necessitatibus quis tempore delectus recusandae dolores animi nostrum sequi suscipit dolor ut adipisci, officiis minima?</p>
+   <img src="https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80 " alt="img"/>
   </>
  )
 }
