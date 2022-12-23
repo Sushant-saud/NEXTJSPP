50a5b8391fe18db14ab5303e0bbb954500a419fa
Sushant-saud <sushantsaud9804@gmail.com>
2022-12-23 18:54:07 +0530

    change done

--- pages/contact.js
+++ pages/contact.js
@@ -5,12 +5,10 @@ export default function Contact() {
    <Title title="Contact" />
    <h1>Contact Page</h1>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem inventore aperiam veniam impedit quas necessitatibus quis tempore delectus recusandae dolores animi nostrum sequi suscipit dolor ut adipisci, officiis minima?</p>
-<<<<<<< HEAD
+
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem inventore aperiam veniam impedit quas necessitatibus quis tempore delectus recusandae dolores animi nostrum sequi suscipit dolor ut adipisci, officiis minima?</p>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem inventore aperiam veniam impedit quas necessitatibus quis tempore delectus recusandae dolores animi nostrum sequi suscipit dolor ut adipisci, officiis minima?</p>
-=======
-   <p>Just try to understand the concept of branching and marging</p>
->>>>>>> d8be3dfb2c97fa411811977bbd9b32be72fb3f96
+     <p>try to understand the concept of branching and marging</p>
   </>
  )
 }
